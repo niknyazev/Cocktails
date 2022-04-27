86a1f2d260e598c1f9988bc9adcb9f848b8f75b0
niknyazev <n.knyazev.dev@gmail.com>
2022-04-27 20:09:42 +0300

    Initial Commit

---
+++ Cocktails/CocktailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsApp.swift
+//  Cocktails
+//
+//  Created by Николай on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CocktailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
