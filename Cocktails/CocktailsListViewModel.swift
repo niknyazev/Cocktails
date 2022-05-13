42a6344f57505779a837b0883c20cce44c95c62b
niknyazev <n.knyazev.dev@gmail.com>
2022-05-13 17:05:18 +0300

    Change project structure

---
+++ Cocktails/CocktailsListViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsListViewModel.swift
+//  Cocktails
+//
+//  Created by Николай on 13.05.2022.
+//
+
+import Foundation
+
+class CocktailsListViewModel: ObservableObject {
+    
+    let cocktails: [CocktailRowViewModel] = []    
+}
+
+class CocktailRowViewModel {
+    
+}
---
+++ Cocktails/FavouriteCocktailsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavouriteCocktailsViewModel.swift
+//  Cocktails
+//
+//  Created by Николай on 13.05.2022.
+//
+
+import Foundation
